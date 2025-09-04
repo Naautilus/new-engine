f5c663f2958a2224cf9cc021f0ed5e5294f84e60
Naautilus <matthewlyubchik@gmail.com>
2025-09-04 14:00:01 -0700

    deleted makefiles

--- build_debug/CMakeFiles/main.dir/src/interface/control_interface.cpp.obj.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/main.dir/src/interface/control_interface.cpp.obj: \
- C:\Users\user\Documents\code_windows\new-engine\src\interface\control_interface.cpp
--- build_debug/CMakeFiles/main.dir/src/interface/model_interface.cpp.obj.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/main.dir/src/interface/model_interface.cpp.obj: \
- C:\Users\user\Documents\code_windows\new-engine\src\interface\model_interface.cpp
