640d67a89084043bff8d8722bcd80ab462de63b4
Naautilus <matthewlyubchik@gmail.com>
2025-09-05 11:32:48 -0700

    updated scenarios and decreased damage

--- src/constants/constants.cpp
+++ src/constants/constants.cpp
@@ -5,11 +5,11 @@ namespace constants {
 const double DELTA_T = 0.005;
 const double LOG_INTERVAL = 1;
 const double TIME_LIMIT = -1;
-const double DAMAGE_MULTIPLIER = 2e-4;
+const double DAMAGE_MULTIPLIER = 1e-4;
 const double UNCONTROLLABLE_HEALTH_FRACTION = 0.8;
 const double SAFE_COLLISION_SPEED = 5;
 const double CYLINDER_VERTICES = 8;
-const double PLANET_RADIUS = 6357000;// * 1e3; // the large multiplier is to make it extremely severe for testing purposes
+const double PLANET_RADIUS = 6357000;
 const double STANDARD_GRAVITY = 9.81;
 
 const double WATER_LEVEL = 5000;
