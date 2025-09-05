9bb6db91f031ce0fc179a4726c47295416795991
Naautilus <matthewlyubchik@gmail.com>
2025-09-05 09:24:29 -0700

    removed aerodynamic_surface's epsilon being a variable

--- src/module/aerodynamic_surface.cpp
+++ src/module/aerodynamic_surface.cpp
@@ -26,7 +26,7 @@ void aerodynamic_surface::update(physics_object::object* parent) {
 }
 void aerodynamic_surface::update_static_surface(physics_object::object* parent) {
     vector::worldspace surface_velocity = parent->physics_state.velocity - parent->physics_state.angular_velocity.cross(position.to_worldspace(parent->physics_state.rotation));
-    if (surface_velocity.squaredNorm() < SPEED_EPSILON) return;
+    if (surface_velocity.squaredNorm() < std::numeric_limits<double>::epsilon()) return;
     //vector::worldspace surface_velocity = parent->physics_state.velocity; // higher performance, a little less accurate
     vector::localspace v = surface_velocity.to_localspace(parent->physics_state.rotation);
     double force = v.dot(unrotated_direction) / v.norm();
@@ -50,7 +50,7 @@ void aerodynamic_surface::update_dynamic_surface(physics_object::object* parent)
     rotated_direction = rotation * unrotated_direction;
 
     vector::worldspace surface_velocity = parent->physics_state.velocity - parent->physics_state.angular_velocity.cross(position.to_worldspace(parent->physics_state.rotation));
-    if (surface_velocity.squaredNorm() < SPEED_EPSILON) return;
+    if (surface_velocity.squaredNorm() < std::numeric_limits<double>::epsilon()) return;
     vector::localspace v = surface_velocity.to_localspace(parent->physics_state.rotation);
     double force = v.dot(rotated_direction) / v.norm();
     vector::localspace force2 = -force * 0.5 * ground::fluid_density(position.to_worldspace_positional(parent->physics_state.rotation, parent->physics_state.position).z()) * surface_area * rotated_direction * v.squaredNorm();
